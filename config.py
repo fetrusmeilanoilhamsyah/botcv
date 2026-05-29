09842d84dc3721d4d77c32e73d6afeacc6ecb392
fetrusmeilanoilhamsyah <fetrusmeilanoilhamsyah@gmail.com>
2026-05-29 16:43:08 +0700

    fix: SEND_BATCH_SIZE 1

--- config.py
+++ config.py
@@ -64,7 +64,7 @@ FILE_READ_TIMEOUT      = 15   # Timeout pembacaan file
 FILE_WRITE_TIMEOUT     = 20   # Timeout penulisan file
 FILE_CONNECT_TIMEOUT   = 10   # Timeout koneksi ke Bot API
 
-SEND_BATCH_SIZE  = 3     # kirim N file lalu napas
+SEND_BATCH_SIZE  = 1     # kirim N file lalu napas
 SEND_BATCH_DELAY = 0.15   # detik napas setelah tiap batch
 SEND_FILE_DELAY  = 0.05  # detik jeda antar file dalam batch
 
