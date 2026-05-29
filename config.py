1e7f9f8228ae35da0552f906ecb94b72e02b8029
fetrusmeilanoilhamsyah <fetrusmeilanoilhamsyah@gmail.com>
2026-05-29 17:22:32 +0700

    tune: SEND_BATCH_DELAY 0.12 SEND_FILE_DELAY 0.06

--- config.py
+++ config.py
@@ -64,9 +64,9 @@ FILE_READ_TIMEOUT      = 15   # Timeout pembacaan file
 FILE_WRITE_TIMEOUT     = 20   # Timeout penulisan file
 FILE_CONNECT_TIMEOUT   = 10   # Timeout koneksi ke Bot API
 
-SEND_BATCH_SIZE  = 1     # kirim N file lalu napas
-SEND_BATCH_DELAY = 0.1   # detik napas setelah tiap batch
-SEND_FILE_DELAY  = 0.08  # detik jeda antar file dalam batch ini kernder tapi 
+SEND_BATCH_SIZE  = 1
+SEND_BATCH_DELAY = 0.12
+SEND_FILE_DELAY  = 0.06
 
 
 
